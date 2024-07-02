7153ac75c0673ac77c15392f7996e6a6019168c9
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-07-02 11:18:09 +0530

    add update post route api

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -82,3 +82,28 @@ export const deletePost = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updatePost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this post'));
+  }
+
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(req.params.postId, {
+      $set : {
+        title: req.body.title,
+        slug: req.body?.title
+          ?.split(' ')
+          .join('-')
+          .toLowerCase()
+          .replace(/[^a-zA-Z0-9-]/g, ''),
+        content: req.body.content,
+        image: req.body.image,
+        category: req.body.category
+      }
+    }, {new : true})
+    res.status(200).json({updatedPost});
+  } catch(err){
+    next(err)
+  }
+}
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import {verifyUser} from '../utils/verifyUser.js'
-import { create, getPosts, deletePost } from '../controllers/post.controller.js';
+import { create, getPosts, deletePost, updatePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUser, create)
 router.get('/getPosts', getPosts)
 router.delete('/deletePost/:postId/:userId', verifyUser, deletePost)
+router.put('/updatePost/:postId/:userId', verifyUser, updatePost)
 
 export default router;
