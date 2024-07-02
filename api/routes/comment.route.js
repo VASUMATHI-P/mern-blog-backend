6a354280e1577bf7629397e60602a52560be9e7e
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-07-02 22:25:01 +0530

    add like functonality to the comments

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,11 @@
 import { verifyUser } from "../utils/verifyUser.js";
 import express from 'express';
-import { createComment, getPostComments } from "../controllers/comment.controller.js";
+import { createComment, getPostComments, likeComment } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.post('/create', verifyUser, createComment);
 router.get('/getpostcomments/:postId', getPostComments);
+router.put('/likecomment/:commentId', verifyUser, likeComment)
 
 export default router;
