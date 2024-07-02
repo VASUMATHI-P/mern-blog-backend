2a21872de949442208a38e61750f9e0a254c01e5
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-07-02 15:35:18 +0530

    add get comments api for the post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,9 +1,9 @@
 import { errorHandler } from "../utils/error.js";
+import Comment from "../model/comment.model.js";
 
 export const createComment = async (req, res, next) => {
   try {
     const {content, postId, userId} = req.body;
-
     if(req.user.id !== userId){
       return next(errorHandler(404, 'Unauthorized'))
     }
@@ -12,5 +12,16 @@ export const createComment = async (req, res, next) => {
   } catch(err){
     next(err);
   }
+}
 
+export const getPostComments = async(req, res, next) => {
+  try{
+    const {postId} = req.params;
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1
+    })
+    res.status(200).json({comments})
+  } catch(err){
+    next(err);
+  }
 }
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,10 @@
 import { verifyUser } from "../utils/verifyUser.js";
 import express from 'express';
-import { createComment } from "../controllers/comment.controller.js";
+import { createComment, getPostComments } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
-router.post("/create", verifyUser, createComment);
+router.post('/create', verifyUser, createComment);
+router.get('/getpostcomments/:postId', getPostComments);
 
 export default router;
