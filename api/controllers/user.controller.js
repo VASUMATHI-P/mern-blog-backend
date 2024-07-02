8a3aa8e16d32b7c9d34996feae61c02cad0361c9
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-07-02 11:58:27 +0530

    create get users api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -77,3 +77,43 @@ export const signout = (req, res, next) => {
     next(err);
   }
 }
+
+export const getUsers = async (req, res, next) => {
+  if(!req.user.isAdmin){
+    return next(errorHandler(403, 'You are not allowed to view all the users'));
+  }
+
+  const startIndex = req.query.startIndex || 0;
+  const limit = req.query.limit || 10;
+  const sortDirection = req.query.order === 'asc' ? 1 : -1;
+  try {
+    const users = await User.find().sort({createdAt: sortDirection}).skip(startIndex).limit(limit)
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    })
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    )
+    const lastMonthUsers = await User.countDocuments({
+      updatedAt : {$gte: oneMonthAgo}
+    })
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers
+    })
+
+  } catch(err){
+    next(err);
+  }
+
+}
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signout } from '../controllers/user.controller.js';
+import { test, updateUser, deleteUser, signout, getUsers } from '../controllers/user.controller.js';
 import { verifyUser } from "../utils/verifyUser.js"
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/test', test)
 router.put('/update/:id', verifyUser, updateUser)
 router.delete('/delete/:id', verifyUser, deleteUser);
 router.post('/signout', signout)
+router.get('/getusers', verifyUser, getUsers)
 
 export default router;
