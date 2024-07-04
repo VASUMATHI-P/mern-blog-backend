9cc0aed84385f4fc2c09adedb4fe0e0708a28e83
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-07-04 11:38:03 +0530

    add comments section to the admin dashboard

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 import { verifyUser } from "../utils/verifyUser.js";
 import express from 'express';
-import { createComment, getPostComments, likeComment, editComment, deleteComment } from "../controllers/comment.controller.js";
+import { createComment, getPostComments, likeComment, editComment, deleteComment, getComments } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get('/getpostcomments/:postId', getPostComments);
 router.put('/likecomment/:commentId', verifyUser, likeComment);
 router.put('/editcomment/:commentId', verifyUser, editComment)
 router.delete('/deletecomment/:commentId', verifyUser, deleteComment)
+router.get('/getcomments', verifyUser, getComments)
 
 export default router;
