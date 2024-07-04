7a765b420dffd35a478cdd210ab3018a8c72074f
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-07-04 10:49:43 +0530

    add delete functionality to the comment

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 import { verifyUser } from "../utils/verifyUser.js";
 import express from 'express';
-import { createComment, getPostComments, likeComment, editComment } from "../controllers/comment.controller.js";
+import { createComment, getPostComments, likeComment, editComment, deleteComment } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
@@ -8,6 +8,6 @@ router.post('/create', verifyUser, createComment);
 router.get('/getpostcomments/:postId', getPostComments);
 router.put('/likecomment/:commentId', verifyUser, likeComment);
 router.put('/editcomment/:commentId', verifyUser, editComment)
-
+router.delete('/deletecomment/:commentId', verifyUser, deleteComment)
 
 export default router;
